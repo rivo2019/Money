bd839e6d455ea65a43ac5e63bf64d33b20d2ad5f
rivo2019 <47436318+rivo2019@users.noreply.github.com>
2019-02-08 02:52:12 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("543209806039744542")
+setInterval(function() {
+channel.send(`|Moneyyy|rw4353456r`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
