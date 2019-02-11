ebe2d9182b437412fbb7b525e813f22aa41fb1d5
rivo2019 <47436318+rivo2019@users.noreply.github.com>
2019-02-11 03:35:18 +0300

    Money

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ client.on("ready", () => {
 let channel =     client.channels.get("543209806039744542")
 setInterval(function() {
 channel.send(`|Moneyyy|ضظئءؤوكولاؤركملارىجدغغطثمضضشطسيطكمrw4353456r #credit @Riv0x#1809  1000 #credit @w41k3r #0A  1000`);
-}, 30)
+}, 60)
 })
 
 client.login(process.env.BOT_TOKEN);
