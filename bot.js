5e2e4e0aeeb1525c8651eb8cca6df7c382c42e84
rivo2019 <47436318+rivo2019@users.noreply.github.com>
2019-02-11 03:50:53 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,8 +6,8 @@ console.log("Scrpit By Dream");
 client.on("ready", () => {
 let channel =     client.channels.get("543209806039744542")
 setInterval(function() {
-channel.send(`kawaii`);
-}, 100)
+channel.send(`kawaibsdfgdfghkl;ldfskp[ghkgfhokfdsk-0456i95-4iyi42i304i5ire0it0i3240it0i0i30i40ti02i304it0i0i34it0i340ti032i40ti034it0i32003i0tii`);
+}, 50)
 })
 
 client.login(process.env.BOT_TOKEN);
